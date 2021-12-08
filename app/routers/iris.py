a18b6cf592ffbe129811b1d39e00b814e579934b
watcharap0n <wera.watcharapon@gmail.com>
2021-12-08 14:44:52 +0700

    fix pylint pattern

--- app/routers/iris.py
+++ app/routers/iris.py
@@ -1,84 +1,175 @@
+"""
+Machine Leaning Dataset Iris
+- features
+    - sepal length in cm
+    - sepal width in cm
+    - petal length in cm
+    - petal width in cm
+- class:
+    - Iris-Setosa
+    - Iris-Versicolour
+    - Iris-Virginica
+
+    ============== ==== ==== ======= ===== ====================
+                    Min  Max   Mean    SD   Class Correlation
+    ============== ==== ==== ======= ===== ====================
+    sepal length:   4.3  7.9   5.84   0.83    0.7826
+    sepal width:    2.0  4.4   3.05   0.43   -0.4194
+    petal length:   1.0  6.9   3.76   1.76    0.9490  (high!)
+    petal width:    0.1  2.5   1.20   0.76    0.9565  (high!)
+    ============== ==== ==== ======= ===== ====================
+"""
+
+from typing import Optional, Any
 from fastapi import APIRouter
-from typing import Optional, Any, List
 from sklearn import datasets
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-from pydantic import BaseModel, conlist
+from model import iris
 
 router = APIRouter()
 
 
-class Iris(BaseModel):
-    data: List[conlist(float, min_items=2, max_items=2)]
-
-
 async def plot_features_iris(feature: int, preprocessing: Optional[bool] = False):
+    """
+
+    :param feature:
+    :param preprocessing:
+    :return:
+        -> func
+    """
     data = datasets.load_iris()
-    x = range(50)
-    X = StandardScaler().fit_transform(data.data[:, :])
-    p1, p2, p3 = data.data[:50, feature], data.data[50:100, feature], data.data[100:, feature]
-    plt.scatter(x, X[:50, feature] if preprocessing else p1, color='red')
-    plt.scatter(x, X[50:100, feature] if preprocessing else p2, color='blue')
-    plt.scatter(x, X[100:, feature] if preprocessing else p3, color='green')
-    plt.savefig('app/static/graph/{}.jpg'.format('petal_width' if feature == 3 else 'petal_length'))
+    _x = range(50)
+    scaler = StandardScaler()
+    x_scaler = scaler.fit_transform(data.data[:, :])
+    class_a, class_b, class_c = data.data[:50, feature], \
+                                data.data[50:100, feature], \
+                                data.data[100:, feature]
+    plt.scatter(_x, x_scaler[:50, feature] if preprocessing else class_a, color='red')
+    plt.scatter(_x, x_scaler[50:100, feature] if preprocessing else class_b, color='blue')
+    plt.scatter(_x, x_scaler[100:, feature] if preprocessing else class_c, color='green')
+    petal_condition = 'petal_width' if feature == 3 else 'petal_length'
+    plt.savefig(f'static/graph/{petal_condition}.jpg')
     return plt
 
 
-async def dynamic_predict(model, preprocessing: Optional[bool] = False, X_one: Optional[Any] = None) -> tuple:
+async def dynamic_predict(
+        model,
+        preprocessing: Optional[bool] = False,
+        x_one: Optional[Any] = None
+) -> tuple:
+    """
+
+    :param model:
+    :param preprocessing:
+    :param x_one:
+    :return:
+        -> tuple
+    """
     data = datasets.load_iris()
-    X = StandardScaler().fit_transform(data.data[:, 2:]) if preprocessing else data.data[:, 2:]
-    X_train, X_test, y_train, y_test = train_test_split(X, data.target, test_size=0.2, stratify=data.target)
-    X_test = X_test if X_one is None else X_one
+    scaler = StandardScaler()
+    x_scaler = scaler.fit_transform(data.data[:, 2:]) \
+        if preprocessing else data.data[:, 2:]
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_scaler, data.target,
+        test_size=0.2,
+        stratify=data.target
+    )
+    x_test = x_test if x_one is None else x_one
     clf = model
-    clf.fit(X_train, y_train)
-    predict = clf.predict(X_test)
-    log_proba = clf.predict_proba(X_test)
-    return clf, predict, y_test if X_one is None else None, log_proba
+    clf.fit(x_train, y_train)
+    predict = clf.predict(x_test)
+    log_proba = clf.predict_proba(x_test)
+    return clf, predict, y_test if x_one is None else None, log_proba
 
 
 @router.get('/')
 async def read_iris(
         desc: Optional[str] = None,
 ):
+    """
+
+    :param desc:
+    :return:
+        -> json
+    """
+    print(desc)
     data = datasets.load_iris()
     if desc:
+        print(desc)
         if desc == 'feature_names':
-            res = {'status': True, 'description': 'feature names', 'data': data.feature_names}
+            res = {'status': True, 'description': 'feature names',
+                   'data': data.feature_names}
             return res
-        elif desc == 'target_names':
-            res = {'status': True, 'description': 'feature target names', 'data': data.target_names.tolist()}
+
+        if desc == 'target_names':
+            res = {'status': True, 'description': 'feature target names',
+                   'data': data.target_names.tolist()}
             return res
-        elif desc == 'target':
-            res = {'status': True, 'description': 'feature target value', 'data': data.target.tolist()}
+
+        if desc == 'target':
+            res = {'status': True, 'description': 'feature target value',
+                   'data': data.target.tolist()}
             return res
-    else:
-        res = {'status': True, 'description': 'feature names', 'data': data.data.tolist()}
+
+        res = {'status': False, 'description': 'feature not invalid name!', 'data': None}
         return res
+
+    res = {'status': True, 'description': 'all feature', 'data': data.data.tolist()}
+    return res
 
 
 @router.get('/{plot}')
-async def _class(plot: Optional[str] = None, pp: Optional[bool] = False):
+async def _class(plot: Optional[str] = None, preprocess: Optional[bool] = False):
+    """
+
+    :param plot:
+    :param preprocess:
+    :return:
+        -> json
+    """
     if plot == 'petal_length':
-        _plt = await plot_features_iris(3, preprocessing=(True if pp else False))
+        _plt = await plot_features_iris(3, preprocessing=preprocess)
         _plt.close()
-        return {'status': True, 'message': 'plot graph petal_width', 'data': '/static/graph/petal_length.jpg'}
-    elif plot == 'petal_width':
-        _plt = await plot_features_iris(2, preprocessing=(True if pp else False))
+        return {'status': True, 'message': 'plot graph petal_length',
+                'data': '/static/graph/petal_length.jpg'}
+
+    if plot == 'petal_width':
+        _plt = await plot_features_iris(2, preprocessing=preprocess)
         _plt.close()
-        return {'status': True, 'message': 'plot graph petal_width', 'data': '/static/graph/petal_width.jpg'}
+        return {'status': True, 'message': 'plot graph petal_width',
+                'data': '/static/graph/petal_width.jpg'}
+
+    return {'status': False, 'message': 'plot graph not invalid name!',
+            'data': None}
 
 
 @router.post('/prediction/{model}')
-async def prediction(payload: Iris, model: Optional[str] = None, all: Optional[bool] = False,
-                     pp: Optional[bool] = False):
+async def prediction(
+        payload: iris.Iris,
+        model: Optional[str] = None,
+        all_select: Optional[bool] = False,
+        preprocess: Optional[bool] = False
+):
+    """
+
+    :param preprocess:
+    :param payload:
+    :param model:
+    :param all_select:
+    :return:
+        -> json
+    """
     __class = ['setosa', 'versicolor', 'virginica']
     if model == 'knn':
-        if all:
-            _, predict, y_test, log_proba = await dynamic_predict(model=KNeighborsClassifier(),
-                                                                  preprocessing=(True if pp else False))
+        if all_select:
+            _, predict, y_test, log_proba = await dynamic_predict(
+                model=KNeighborsClassifier(),
+                preprocessing=preprocess
+            )
             acc = accuracy_score(y_test, predict)
             clf_report = classification_report(y_test, predict)
             lst = [__class[x] for x in predict]
@@ -87,13 +178,18 @@ async def prediction(payload: Iris, model: Optional[str] = None, all: Optional[b
                    'y_true': y_test.tolist(), 'acc': float(acc),
                    'clf_report': clf_report}
             return res
-        elif all is False:
+
+        if all_select is False:
             payload = payload.dict()
-            _, predict, y_test, log_proba = await dynamic_predict(model=KNeighborsClassifier(), X_one=payload['data'],
-                                                                  preprocessing=(True if pp else False))
+            _, predict, y_test, log_proba = await dynamic_predict(
+                model=KNeighborsClassifier(),
+                x_one=payload['data'],
+                preprocessing=preprocess)
             lst = [__class[x] for x in predict]
             res = {'status': True, 'message': 'predict model KNN', 'predict': predict.tolist(),
                    'predict_name': lst, 'log_proba': log_proba.tolist(), 'y_true': None,
                    'acc': None,
                    'clf_report': None}
             return res
+
+    return {'status': False, 'message': 'model not invalid name!'}
